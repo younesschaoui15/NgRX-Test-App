1efc6c95c28e9891fd34905a3d221a30c9985231
Youness CHAOUI <younesschaoui15@gmail.com>
2019-02-08 11:37:33 +0100

    tutos list components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListTutosComponent} from './components/list-tutos/list-tutos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListTutosComponent, pathMatch: 'full'},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
